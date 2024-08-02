0422ca3424b15b05b79edbb48ddcc50d834084a9
N01Knowz <ronronarnibal@gmail.com>
2024-08-02 21:58:02 +0800

    Update

--- src/page/Posts.vue
+++ src/page/Posts.vue
@@ -21,15 +21,15 @@ watch(posts, (newValue) => {
 </script>
 
 <template>
-  <div class="flex gap-12 items-center justify-center w-full bg-black flex-wrap p-6">
+  <div class="flex gap-12 items-center justify-center w-full flex-wrap p-6">
     <span v-if="!posts" class="loading loading-spinner loading-xs"></span>
     <div
       v-if="posts"
       v-for="post in posts"
-      class="card w-96 bg-base-100 shadow-xl h-64"
+      class="card w-96 bg-base-100 shadow-info-300 shadow-xl h-64"
     >
       <div class="card-body">
-        <h2 class="card-title">{{ post.title }}</h2>
+        <h2 class="card-title border-primary border-b-[1px]">{{ post.title }}</h2>
         <p>{{ post.body }}</p>
       </div>
     </div>
